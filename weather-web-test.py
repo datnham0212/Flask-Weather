0af02d2aa22deb22c13f2cea9ec605b6388310b8
datnham0212 <90122319+datnham0212@users.noreply.github.com>
2024-10-14 11:19:12 +0700

    Add files via upload

--- weather-web-test.py
+++ weather-web-test.py
@@ -1,79 +1,29 @@
-from flask import Flask, render_template, request    
+from flask import Flask, render_template
+import datetime
 import requests 
 # redirect, url_for, json
 app = Flask(__name__)
 
-my_API_key = ""
+my_API_key = "d29ca490f40cf3c78c2e2895f0f3b62c"
+cityName = 'New York'
 
-@app.route("/", methods=['POST', 'GET'])
-def home():
-    if request.method == 'POST':
-        # return render_template('index.html', content = info(request.form.get('cityName')))
-        latitude = request.form.get('latitude')
-        longitude = request.form.get('longitude')
-
-        if latitude and longitude:
-            content = info(latitude, longitude);
-
-        else:
-            content = '<p>Please select a location on the map...</p>';
-
-        return render_template('index.html', content = content);
-    else:
-        return render_template('index.html', content = '')
-
-def info(latitude, longitude):
-    response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={my_API_key}')
+def get_weather_data(cityName):
+    c = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={my_API_key}').json()
+    response = requests.get(f'https://api.openweathermap.org/data/2.5/forecast?lat={c['coord']['lat']}&lon={c['coord']['lon']}&appid={my_API_key}')
     details = response.json()
-    table = f"""
-    <table border="1">
-        <tr>
-            <th colspan="2">Weather Details</th>
-        </tr>
-        <tr>
-            <td>City</td>
-            <td>{details['name']}</td>
-        </tr>
-        <tr>
-            <td>Country</td>
-            <td>{details['sys']['country']}</td>
-        </tr>
-        <tr>
-            <td>Temperature</td>
-            <td>{details['main']['temp'] - 273.15:.2f} °C</td>
-        </tr>
-        <tr>
-            <td>Feels Like</td>
-            <td>{details['main']['feels_like'] - 273.15:.2f} °C</td>
-        </tr>
-        <tr>
-            <td>Humidity</td>
-            <td>{details['main']['humidity']} %</td>
-        </tr>
-        <tr>
-            <td>Description</td>
-            <td>{details['weather'][0]['description']}</td>
-        </tr>
-        <tr>
-            <td>Wind Speed</td>
-            <td>{details['wind']['speed']} m/s</td>
-        </tr>
-        <tr>
-            <td>Cloudiness</td>
-            <td>{details['clouds']['all']} %</td>
-        </tr>
-        <tr>
-            <td>Sunrise</td>
-            <td>{details['sys']['sunrise']}</td>
-        </tr>
-        <tr>
-            <td>Sunset</td>
-            <td>{details['sys']['sunset']}</td>
-        </tr>
-    </table>
-    """
-    return table
+    return details
 
+@app.route("/")
+def city_info():
+    city_data = get_weather_data(cityName)['city']
+    city_data['sunrise'] = datetime.datetime.fromtimestamp(city_data['sunrise'])
+    city_data['sunset'] = datetime.datetime.fromtimestamp(city_data['sunset'])
+    return render_template("index.html", city=city_data)
+
+@app.route("/forecast")
+def forecast():
+    forecast = get_weather_data(cityName)['list']
+    return render_template('forecast.html', weather_data=forecast)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
